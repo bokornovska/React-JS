3f43f65b17517456913e3d83c01d991b485635ae
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-03-27 18:58:54 +0300

    Routing - workshop

---
+++ 07. Workshop - Routing/react-games-play/src/App.js
@@ -0,0 +1,60 @@
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import {useState, useEffect} from 'react';
+
+import * as gameService from './services/gameService'
+import { Catalog } from "./components/Catalog/Catalog";
+import { Create } from "./components/Create/Create";
+// import { Edit } from "./components/Edit/Edit";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { GameDetails } from './components/GameDetails/GameDetails';
+
+function App() {
+
+    const navigate = useNavigate()
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+        .then(result => {
+            setGames(result)
+        })
+    }, []);
+
+    const onCreateGameSubmit = async (data) => {
+        
+        const newGame = await gameService.create(data);
+
+        //todo add to state
+
+        setGames(state => [...state, newGame])
+        //todo redirect
+        navigate('/catalog')
+    }
+
+    return (
+        <div id="box">
+
+            <Header />
+
+            <main id="main-content">
+                <Routes>
+                    <Route path='/' element={<Home/>} />
+                    <Route path='/login' element={<Login/>} />
+                    <Route path='/register' element={<Register/>} />
+                    <Route path='/create-game' element={<Create onCreateGameSubmit={onCreateGameSubmit}/>} />
+                    <Route path='/catalog' element={<Catalog games={games}/>} />
+                    <Route path='catalog/:gameId' element={<GameDetails/>} />
+                </Routes>
+
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ 07. Workshop - Routing/react-games-play/src/Resource.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div id="box">
+
+        {/* <!-- Create Page ( Only for logged-in users ) --> */}
+       
+
+        {/* <!-- Edit Page ( Only for the creator )--> */}
+       
+
+        {/* <!--Details Page--> */}
+        
+
+        {/* <!-- Catalogue --> */}
+
+    </div>
+  );
+}
+
+export default App;
